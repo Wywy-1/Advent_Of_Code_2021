90499e77ed0355231cdd2c4489d6d9929590e0a2
Wyatt Schiefelbein <schiefel@ualberta.ca>
2022-09-11 15:08:32 -0600

    Rename Files

---
+++ day_1b.py
@@ -0,0 +1,61 @@
+from .day_1 import get_depth_data
+
+def add_three(data, i):
+    '''Adds three consecutive items in a list together, and returns 
+    their sum.
+    Inputs:     list, starting index
+    Returns:    a number'''
+    sum = 0
+
+    for n in range(i, i+3):
+        sum += data[n]
+
+    print(sum)
+
+    return sum
+
+
+def compare(data, i):
+    '''Compares the sum of three consecutive list items to 
+    the sum of another three consecutive list items, one removed from
+    the first.
+
+    Inputs:     list, starting index
+    returns:    True if first three is smaller the next three
+                False if first three is larger.'''
+
+    return add_three(data, i) < add_three(data, i+1)
+
+
+def iterate_depth_data(data):
+    '''Iterates through a list of ints and calls compare() 
+    to compare three consecutive items by the following three
+    consecutive numbers. Returns the number of times the former
+    is a smaller number than the latter.
+
+    Inputs:     list
+    Returns:    a number'''
+
+    num = 0
+
+    for n in range(len(data)-3):
+        if compare(data, n):
+            num += 1
+        print('')
+
+    return num
+
+
+def main():
+    '''Initializes the data list and prints results of
+    iterate_depth_data'''
+    data = []
+    #fileName = 'TestInput'
+    fileName = 'input_day_1'
+
+    data = get_depth_data(fileName)
+    print(iterate_depth_data(data))
+
+
+if __name__ == "__main__":
+    main()

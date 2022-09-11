f29f689df2468b688cbe9a0bb3f3860f24e89760
Wyatt Schiefelbein <schiefel@ualberta.ca>
2022-09-11 15:44:32 -0600

    Added functions:
    1) update_vector, and
    2) modifiers to class Vector

--- day_2.py
+++ day_2.py
@@ -1,10 +1,47 @@
-class Vector:
-    '''Represents direction and degree of movement'''
-    #Constructor
-    def __init__(self, hor, vir, num):
-        self.hor = hor
-        self.vir = vir
-        self.num = num
+from abc import update_abstractmethods
 
-sub = Vector(0,0,0)
+
+class Vector:
+    '''Represents direction and degree of movement from a starting 
+    point (0,0)'''
+
+    #Constructor
+    def __init__(token, hor, vir):
+        token.hor = hor     #i.e., "forward"
+        token.vir = vir     #i.e., "down" and "-1(up)"
+    
+    #Modifiers
+    def add_hor(self, num):
+        self.hor += num
+
+
+    def add_vir(self, num):
+        self.vir += num
+
+
+def update_vector(sub, dir, degree):
+    '''Modifies a vector-class object when given a 
+    direction (forward, up, or down) and a degree of movement
+    Inputs:     vector object, string, num'''
+
+    if (dir == "forward"):
+        sub.add_hor(degree)
+        #print(f'Going forward {degree} units: sub.hor = {sub.hor}')    #test
+    elif (dir == "down"):
+        sub.add_vir(degree)
+        #print(f'Going down {degree} units: sub.vir = {sub.vir}')   #test
+    else:
+        sub.add_vir(-1*degree)
+        #print(f'Going up {degree} units: sub.vir = {sub.vir}') #test
+
+
+sub = Vector(0,0)
+
+''' test: update_vector 
+update_Vector(sub, "forward", 5)
+update_Vector(sub, "forward", 12)
+update_Vector(sub, "down", 7)
+update_Vector(sub, "up", 2)
+'''
+
 #print("Vector for submarine is: ", sub.hor, sub.vir, sub.num)

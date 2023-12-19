c77bd2e097b11a31504c58ac415119251d950d77
Wyatt Schiefelbein <schiefel@ualberta.ca>
2023-12-19 14:39:47 -0700

    to change things I'm not too sure, it's been a while.

--- day_3.py
+++ day_3.py
@@ -3,11 +3,13 @@ def calc_gamma():
     from a .txt file.
     
     TODO #1 Input: file name
-    Output: list of integers, gamma'''
 
-    test_file = ['1100','1110','1111','1101']
+    Output: string'''
+
+    test_file = ['1100','1110','1111','1100']
     
-    gamma = [0,0,0,0]
+    gamma = ''
+    gamma_intlist = [0,0,0,0]
     gamma_float = [0.0,0.0,0.0,0.0]
 
     word_counter = 0
@@ -15,23 +17,33 @@ def calc_gamma():
     # Adds the value of bits in corresponding indices across all words in test_file
     #   Also counts the number of words (lines) in test file, records in word_counter
     for line in test_file:
+        # Enumerate gives each element of line an index. "count" is the index, "bit_value"
+        #   is the value at that index
+        #   
         for count, bit_value in enumerate(line):
-            gamma[count] += int(bit_value)
+            gamma_intlist[count] += int(bit_value)
         word_counter += 1
     
-    # Averages the raw numbers in each index of bit_tracker by number of words.
-    #   Uses these averages to discern which occurs more often, 0 or 1 for
-    #   each index across all words.
-    print('Calculating averages of each bit index:')
+    print("At 27, after first for-loop in calc_gamma, gamma_intlist is:")
+    print(*gamma_intlist)
 
-    for count,value in enumerate(gamma):
+    # Averages the raw numbers in each index of gamma_intlist by number of words.
+    #   Uses these averages to discern which occurs more often, 0 or 1, for
+    #   each index across all words, stores in gamma_intlist
+    #   TODO #3 Discern how to best address cases where equal 1 and 0, currently
+    #       you're assigning those indices with a "2".
+    for count,value in enumerate(gamma_intlist):
         gamma_float[count] = value/word_counter
         if (gamma_float[count] < 0.5):
-            gamma[count] = 0
+            gamma_intlist[count] = 0
+        elif (gamma_float[count] > 0.5):
+            gamma_intlist[count] = 1
         else:
-            gamma[count] = 1
+            gamma_intlist[count] = 2
     
-    print('{}'.format(gamma))
+    # convert integer list, gamma_int_list, into string, gamma
+    for num in gamma_intlist:
+        gamma += str(num)
 
     return gamma
 
@@ -40,17 +52,18 @@ def calc_epsilon():
     '''Note, since epsilon is just the compliment of gamma
     you just need to inverse each place on gamma, 
     bitwise NOT'''
-    eps = 0b000000000000
+    eps = 0
 
     return eps
 
-def calc_pwr_consum(gamm, epsilon):
+
+def calc_pwr_consum(gamma):
     gamma = calc_gamma()
 
-    print('gamma in decimal is\t%d' % gamma)
-    print('gamma in binary is\t%s' % bin(gamma))
+    print('gamma in binary is\t{}'.format(gamma))
+    # TODO #2 Convert gamma (str) into decimal number
 
 
 if __name__ == "__main__":
-    #calc_pwr_consum(0,0)
-    calc_gamma()   #test
+    calc_pwr_consum(0)
+    #calc_gamma()   #test

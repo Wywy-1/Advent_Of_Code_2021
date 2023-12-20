94f3562017bbe4e619fb5b64cbd3e18bdb1ae61b
Wyatt Schiefelbein <schiefel@ualberta.ca>
2023-12-19 22:18:47 -0700

    style: ammend whitespace and line-breaks
    These changes improve readibility and align with convention.

--- day_3.py
+++ day_3.py
@@ -1,109 +1,89 @@
 def get_pwr_data(fileName):
-    '''Extracts data taken from a file, "fileName" 
+    '''Extracts data taken from a file, "fileName"
+    Input:      a string 
     Returns:    a list[str]'''
+
     data: list[str] = []
     with open(fileName, encoding='utf-8') as file:
-
         for line in file:
             data.append(str(line))
-
     return data
 
 
-def calc_gam_eps():
-    '''Calculates the most common bit in each index of a series of binary words (x12 bits) 
-        from a .txt file.
-        Returns: two int-lists'''
-
-    pwr_data = get_pwr_data('input_day_3.txt')
-    #pwr_data = ['00100','11110','10110','10111','10101','01111','00111','11100','10000','11001','00010','01010']
-
-    #gamma = [0,0,0,0,0]
-    #indices_averages = [0.0,0.0,0.0,0.0,0.0]
-
-
-    gamma = [0,0,0,0,0,0,0,0,0,0,0,0]
-    epsilon = gamma
-    indices_averages = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    word_counter = 0
-    
-    # Adds the value of bits in corresponding indices across all words in test_file,
-    #   saves to gamma_intlist
-    #   Also counts the number of words (lines) in test file, records in word_counter
-    for line in pwr_data:
-        for x in range(12):
-            gamma[x] += int(line[x])
-        word_counter += 1
-
-    # Averages the raw numbers in each index of gamma_intlist by number of words/lines.
-    #   Uses these averages to discern which occurs more often, 0 or 1, for
-    #   each index across all words, stores in gamma_intlist.
-    #   TODO #3 Discern how to best address cases where 1 and 0 occur equally 
-    #       often, currently you're assigning those indices with a "1".
-    for count,value in enumerate(gamma):
-        indices_averages[count] = value/word_counter
-        if (indices_averages[count] < 0.5):
-            gamma[count] = 0
-        elif (indices_averages[count] > 0.5):
-            gamma[count] = 1
-        else:   # When 0 and 1 occur equally as often
-            gamma[count] = 2
-    
-    epsilon = find_compliment(gamma)
-
-    return gamma, epsilon
-
-
-def intList2string(x): 
-    '''Converts an integer-list into a string.'''
-
-    con_string=''
-
-    for num in x:
-        con_string += str(num)
-
-    return con_string
-
-
 def find_compliment(gamma):
-    '''Takes an int-list and returns an int-list
-    representing the former's compliment: examines
-    each index of the former and replaces 0s with
-    1s and 1s with 0s in the latter.
+    '''Takes an int-list of 0s and 1s and returns an int-list representing
+    the former's compliment: That is, examines each index of the former and
+    replaces 0s with 1s and 1s with 0s, then stores these in the latter.
     Input: int-list
     Return: int-list'''
-    eps = []
 
+    eps = []
     for x in gamma:
         if x == 0:
             eps.append(1)
         else:
             eps.append(0)
-
     return eps
+
+
+def calc_gam_eps():
+    '''Calculates the most common bit, 0 or 1, for each index across a
+    series of binary words (x12 bits) taken from a .txt file.
+        Returns: two int-lists'''
+
+    pwr_data = get_pwr_data('input_day_3.txt')
+    gamma = [0,0,0,0,0,0,0,0,0,0,0,0]
+    epsilon = gamma
+    indices_averages = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    word_counter = 0
+    
+    # Adds the value of the bits in corresponding indices across all words 
+    #   in test_file, saves to gamma_intlist.
+    #   Also counts the number of words (lines) in test file, records in 
+    #       word_counter
+    for line in pwr_data:
+        for x in range(12):
+            gamma[x] += int(line[x])
+        word_counter += 1
+
+    # Divides the values stored in each index of gamma_intlist by the number
+    # of words taken from the text file to get the average of values for
+    # each index. Then uses these averages to discern which occurs more 
+    # often, 0 or 1, for each index across all words. Stores this 
+    # information in gamma_intlist.
+    for count,value in enumerate(gamma):
+        indices_averages[count] = (value/word_counter)
+        if (indices_averages[count] < 0.5):
+            gamma[count] = 0
+        elif (indices_averages[count] > 0.5):
+            gamma[count] = 1
+        else:   # When 0 and 1 occur equally as often
+            gamma[count] = 1
+    
+    epsilon = find_compliment(gamma)
+    return gamma, epsilon
 
 
 def calc_pwr_consum(gamma, epsilon):
     '''Calculates the power consumption of the christmas submarine and
     prints the results to terminal.
     Input: int-list, int-list'''
+
     gamma, epsilon = calc_gam_eps()
 
-    bin_gamma = "".join(format(x,'b') for x in gamma)   # Converts each int in int-list to binary.
-    dec_gamma = int(bin_gamma, 2)                       # Converts bin_gamma, as one number, 
-                                                        #   to a decimal integer.
+    # Convert each int in an int-list, gamma and epsilon, to binary.
+    binary_gamma = "".join(format(x,'b') for x in gamma)
+    binary_epsi = "".join(format(x,'b') for x in epsilon)
 
-    bin_epsi = "".join(format(x,'b') for x in epsilon)
-    dec_epsi = int(bin_epsi, 2)
+    # Converts binary_gamma and binary_epsi into singular decimal integers
+    decimal_gamma = int(binary_gamma, 2)
+    decimal_epsi = int(binary_epsi, 2)
 
-    print("Printing binary gamma: {}".format(bin_gamma))
-    print("Printing decimal gamma: {}\n".format(dec_gamma))
-    print("Printing binary epsilon: {}".format(bin_epsi))
-    print("Printing decimal epsilon: {}\n".format(dec_epsi))
-
-    print("Your power consumption is {}\n".format(dec_gamma*dec_epsi))
-
-    # TODO #2 Convert gamma (str) into decimal number
+    print("Printing binary gamma: {}".format(binary_gamma))
+    print("Printing decimal gamma: {}\n".format(decimal_gamma))
+    print("Printing binary epsilon: {}".format(binary_epsi))
+    print("Printing decimal epsilon: {}\n".format(decimal_epsi))
+    print("Your power consumption is {}\n".format(decimal_gamma * decimal_epsi))
 
 
 if __name__ == "__main__":

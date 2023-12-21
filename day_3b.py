43a6d3d002b20078d730bdb7f8a3c6d037121892
Wyatt Schiefelbein <schiefel@ualberta.ca>
2023-12-20 23:08:19 -0700

    style: cleaning comments, names, and whitespace

--- day_3b.py
+++ day_3b.py
@@ -1,7 +1,7 @@
 from day_3 import get_pwr_data
 
 
-def find_least(index_record_0, index_record_1):
+def find_smaller(index_record_0, index_record_1):
     '''Returns the smaller of either index_record_0 or index_record_1.
     Will return index_record_0 if index_record_0 and index_record_1 are
     equal.
@@ -14,7 +14,7 @@ def find_least(index_record_0, index_record_1):
         return index_record_1
 
 
-def find_most(index_record_0, index_record_1):
+def find_larger(index_record_0, index_record_1):
     '''Returns the larger of either index_record_0 or index_record_1.
     Will return index_record_1 if index_record_0 and index_record_1 are
     equal.
@@ -31,7 +31,7 @@ def sort_to_0_1 (data, index, least_most):
     '''Sorts items in data into two lists: One where indices have a 0 at
     index, and one where indices have a 1 at index.
     Calls find_most if least_most == "most", and find_least otherwise.
-    Returns a list with either the items that had the most 1s or 0s at 
+    Returns a list with either the items that had the most 1s or 0s at
     index, or the least, respectively.
     Input:  list[str], int, string
     Returns:    list[str]'''
@@ -48,18 +48,17 @@ def sort_to_0_1 (data, index, least_most):
             index_record_1.append(item)
 
     if least_most == 'most':
-        return find_most(index_record_0, index_record_1)
+        return find_larger(index_record_0, index_record_1)
     else:
-        return find_least(index_record_0, index_record_1)
+        return find_smaller(index_record_0, index_record_1)
 
 
 def find_ratings():
     data = get_pwr_data('input_day_3.txt')
-    
     ogr: list(str) = []     # "Oxygen Generator Rating"
     co2sr: list(str) = []   # "CO2 Scrubber Rating"
 
-    # Compute first-case for OGR and CO2SR using test_data
+    # Compute first-case for OGR and CO2SR using data
     ogr = sort_to_0_1(data, 0, 'most')
     co2sr = sort_to_0_1(data, 0, 'least')
     
@@ -67,15 +66,13 @@ def find_ratings():
     #   OGR rating.
     for count in range(len(data)):
         ogr = sort_to_0_1(ogr, count, 'most')
-        if len(ogr) == 1:   # Break when there is only one item left
-            break
+        if len(ogr) == 1: break
 
     # Subsequent cases of CO2SR similarly feed into item_at_index_least
     #   until we reach the CO2SR rating.
     for count in range(1,len(data)):
         co2sr = sort_to_0_1(co2sr, count, 'least')
-        if len(co2sr) <= 1:   # Break when there is only one item left
-            break
+        if len(co2sr) <= 1: break
 
     print('Oxygen Generator Rating\t{}'.format(ogr))
     print('CO2 Scrubber Rating\t{}'.format(co2sr))

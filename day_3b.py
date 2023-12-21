15fbf9cb821de55a35e89feb60e92a9b52ba0b54
Wyatt Schiefelbein <schiefel@ualberta.ca>
2023-12-20 23:03:37 -0700

    feat: sort and find ogr, co2sr, and lsr

--- day_3b.py
+++ day_3b.py
@@ -1,7 +1,41 @@
-#from day_3 import get_pwr_data
+from day_3 import get_pwr_data
 
-def item_at_index_most(data, index):
-    
+
+def find_least(index_record_0, index_record_1):
+    '''Returns the smaller of either index_record_0 or index_record_1.
+    Will return index_record_0 if index_record_0 and index_record_1 are
+    equal.
+    Input:  list[str], list[str]
+    Returns:    list[str]'''
+
+    if len(index_record_0) <= len(index_record_1):  # If ir0 is smaller or equal       
+        return index_record_0
+    else:
+        return index_record_1
+
+
+def find_most(index_record_0, index_record_1):
+    '''Returns the larger of either index_record_0 or index_record_1.
+    Will return index_record_1 if index_record_0 and index_record_1 are
+    equal.
+    Input:  list[str], list[str]
+    Returns:    list[str]'''
+
+    if len(index_record_0) > len(index_record_1):
+        return index_record_0
+    else:   # Where equal, keep the number with a "1"
+        return index_record_1
+
+
+def sort_to_0_1 (data, index, least_most):
+    '''Sorts items in data into two lists: One where indices have a 0 at
+    index, and one where indices have a 1 at index.
+    Calls find_most if least_most == "most", and find_least otherwise.
+    Returns a list with either the items that had the most 1s or 0s at 
+    index, or the least, respectively.
+    Input:  list[str], int, string
+    Returns:    list[str]'''
+
     index_record_0 = []
     index_record_1 = []
 
@@ -13,98 +47,45 @@ def item_at_index_most(data, index):
         else:
             index_record_1.append(item)
 
-    # Returns the larger of either index_record_0 or index_record_1
-    if len(index_record_0) > len(index_record_1):
-        return index_record_0
+    if least_most == 'most':
+        return find_most(index_record_0, index_record_1)
     else:
-        return index_record_1
+        return find_least(index_record_0, index_record_1)
 
 
-def place_holder():
-    test_data = ['100100110110',
-                '101110110110',
-                '010100010100',
-                '011001110000',
-                '000000000111',
-                '000010110001',
-                '001111000001',
-                '100010000001',
-                '010100110011',
-                '010000010110',
-                '010000000011',
-                '010101001000']
+def find_ratings():
+    data = get_pwr_data('input_day_3.txt')
     
-    ogr: list(str) = []
-    ogr = item_at_index_most(test_data, 0)
-    
-    print('ogr in place_holder now consists of:\t{}'.format(ogr))
-    #co2sr: str = []
+    ogr: list(str) = []     # "Oxygen Generator Rating"
+    co2sr: list(str) = []   # "CO2 Scrubber Rating"
 
-    #print('First item in test data:\t\t\t{}'.format(test_data[0]))
-    #print('First index of first item in test data:\t\t{}'.format(test_data[0][0]))
-    #print('Second index of first item in test data:\t{}'.format(test_data[0][1]))
+    # Compute first-case for OGR and CO2SR using test_data
+    ogr = sort_to_0_1(data, 0, 'most')
+    co2sr = sort_to_0_1(data, 0, 'least')
+    
+    # Subsequent cases, OGR feeds into item_at_index_most until we reach the
+    #   OGR rating.
+    for count in range(len(data)):
+        ogr = sort_to_0_1(ogr, count, 'most')
+        if len(ogr) == 1:   # Break when there is only one item left
+            break
+
+    # Subsequent cases of CO2SR similarly feed into item_at_index_least
+    #   until we reach the CO2SR rating.
+    for count in range(1,len(data)):
+        co2sr = sort_to_0_1(co2sr, count, 'least')
+        if len(co2sr) <= 1:   # Break when there is only one item left
+            break
+
+    print('Oxygen Generator Rating\t{}'.format(ogr))
+    print('CO2 Scrubber Rating\t{}'.format(co2sr))
+
+    ogr = int(ogr[0],2)
+    co2sr = int(co2sr[0],2)
+
+    lsr = ogr * co2sr
+    print('life support rating is\t{}'.format(lsr))
 
 
 if __name__ == "__main__":
-    place_holder()
-
-#oxygen generator rating (OGR)
-
-#To find oxygen generator rating, determine the most common value (0 or
-#1) in the current bit position, and keep only numbers with that bit in
-#that position. If 0 and 1 are equally common, keep values with a 1 in
-#the position being considered.
-
-#E.G: Start with all 12 numbers and consider only the first bit of each
-# number. There are more 1 bits (7) than 0 bits (5), so keep only the 7
-# numbers with a 1 in the first position: 11110, 10110, 10111, 10101,
-# 11100, 10000, and 11001.
-#   Then, consider the second bit of the 7 remaining numbers: there are
-#       more 0 bits (4) than 1 bits (3), so keep only the 4 numbers 
-#       with a 0 in the second position: 10110, 10111, 10101, and 
-#       10000.
-#   In the third position, three of the four numbers have a 1, so keep
-#       those three: 10110, 10111, and 10101.
-#   In the fourth position, two of the three numbers have a 1, so keep
-#       those two: 10110 and 10111.
-#   In the fifth position, there are an equal number of 0 bits and 1
-#       bits (one each). So, to find the oxygen generator rating, keep
-#       the number with a 1 in that position: 10111.
-#   As there is only one number left, stop; the oxygen generator rating
-#       is 10111, or 23 in decimal.
-
-
-
-#CO2 scrubber rating (CO2SR)
-
-#To find CO2 scrubber rating, determine the least common value (0 or 1)
-#in the current bit position, and keep only numbers with that bit in
-#that position. If 0 and 1 are equally common, keep values with a 0 in
-#the position being considered.
-
-#E.G: Start again with all 12 numbers and consider only the first bit of each number. There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a 0 in the first position: 00100, 01111, 00111, 00010, and 01010.
-#   Then, consider the second bit of the 5 remaining numbers: there are
-#       fewer 1 bits (2) than 0 bits (3), so keep only the 2 numbers 
-#       with a 1 in the second position: 01111 and 01010.
-#   In the third position, there are an equal number of 0 bits and 1
-#       bits (one each). So, to find the CO2 scrubber rating, keep the
-#       number with a 0 in that position: 01010.
-#   As there is only one number left, stop; the CO2 scrubber rating is
-#       01010, or 10 in decimal.
-
-
-#Both the oxygen generator rating and the CO2 scrubber rating are 
-#values that can be found in your diagnostic report - finding them is
-#the tricky part. Both values are located using a similar process that
-#involves filtering out values until only one remains. Before 
-#searching for either rating value, start with the full list of binary
-#numbers from your diagnostic report and consider just the first bit
-#of those numbers. Then:
-#Keep only numbers selected by the bit criteria for the type of rating
-#value for which you are searching. Discard numbers which do not match
-#the bit criteria. If you only have one number left, stop; this is the
-#rating value for which you are searching. Otherwise, repeat the
-#process, considering the next bit to the right.
-
-#Multiply the OSR and CO2SR to get Life Support Rating. Of the first
-#   12 numbers in the input, this number should be 230 (decimal)
+    find_ratings()
